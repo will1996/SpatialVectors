8e0e0f6d191d858c45bcbaa59a13ad10a9f256c7
Will Chambers <Will.chambers96@gmail.com>
2019-12-03 22:59:19 -0500

    all base files

---
+++ build/CMakeFiles/spatialVector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spatialVector.dir/spatialVector.o"
+  "libspatialVector.a"
+  "libspatialVector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spatialVector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
